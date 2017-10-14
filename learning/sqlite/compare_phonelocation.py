ffb66a7e036c6e35a60280dd84966c72e12af626
Ubuntu <youthdou@126.com>
2017-10-14 03:31:22 -0700

    modify

--- learning/sqlite/compare_phonelocation.py
+++ learning/sqlite/compare_phonelocation.py
@@ -4,8 +4,8 @@ import sqlite3
 import xlrd
 import time
 import requests
-#from bs4 import BeautifulSoup
-#import html5lib
+from bs4 import BeautifulSoup
+import html5lib
 
 import threading
 import queue
@@ -13,18 +13,72 @@ import random
 import time
 import multiprocessing
 
+bRunning = True
+
+iMaxCnt = 0
+
+lock = threading.Lock()
+
+def queryNumberV2(strNumber):
+    url = 'http://www.ip138.com:8080/search.asp?action=mobile&mobile=%s' % (strNumber)
+    r = requests.get(url)
+    content = r.content.decode('gb2312')
+    bsObj = BeautifulSoup(content, 'html5lib')
+    listInfos = bsObj.find_all('td', {'class':'tdc2'})
+    result = {}
+    result['QueryResult'] = 'False'
+    if len(listInfos) != 5:
+        print(r.url)
+        return result
+
+    result['Mobile'] = strNumber
+    result['QueryResult'] = 'True'
+    strLocation = listInfos[1].getText()
+    #print(strLocation, ':', len(strLocation.split('\xa0')))
+    #print(list(strLocation))
+
+    result['Province'] = strLocation.split('\xa0')[0]
+    result['City'] = strLocation.split('\xa0')[1].rstrip('市')
+    result['AreaCode'] = listInfos[3].getText()
+    result['PostCode'] = listInfos[4].getText().split(' ')[0]
+    result['VNO'] = ''
+    result['Corp'] = listInfos[2].getText()
+
+    if listInfos[2].getText().find('联通') != -1:
+        result['TO'] = '联通'
+    elif listInfos[2].getText().find('电信') != -1:
+        result['TO'] = '电信'
+    elif listInfos[2].getText().find('移动') != -1:
+        result['TO'] = '移动'
+    else:
+        result['TO'] = ''
+
+
+    #print(result)
+    return result
+
 
 def queryNumber(strNumber):
+    result = {}
+    result['QueryResult'] = 'False'
+    with lock:
+        global iMaxCnt
+        if iMaxCnt > 100:
+            iMaxCnt = iMaxCnt + 1
+            return result
+
+    time.sleep(2)
     url = 'http://v.showji.com/Locating/showji.com2016234999234.aspx?m=%s&output=json&callback=querycallback&timestamp=%d' % (strNumber, time.time())
     r = requests.get(url)
     #print(r.url)
     #print(r.text)
     #print(r.status_code)
-    result = {}
+
     if r.text.strip().startswith('querycallback('):
         result = eval(r.text.lstrip('querycallback(').rstrip(');'))
     else:
-        print(r.url)
+        print('[%d]%s' % (iMaxCnt, r.url))
+        return queryNumberV2(strNumber)
     #for key in result:
     #    print('%s:%s' % (key, result[key]))
     return result
@@ -37,83 +91,99 @@ def getTypes(details):
     return details.get('Corp', '') + ' ' + details.get('VNO')
 
 
-def updateRecord(cursor, strNumber, strInfos, file):
+def updateRecord(strNumber, strInfos):
     details = queryNumber(strNumber)
     if details.get('QueryResult', 'False') == 'False':
-        file.write(strNumber + ',' + strInfos + ',\n')
-        return
+        #file.write(strNumber + ',' + strInfos + ',\n')
+        return (False, 'update,' + strNumber + ',' + strInfos + ',\n')
 
+    #print(strInfos)
     strProvince, strCity, strIsp = strInfos.split('#')
     if strProvince != details.get('Province', '').strip():
-        file.write(strNumber + ',' + strInfos + ',\n')
-        return
+        return (False, 'update,' + strNumber + ',' + strInfos + ',\n')
     if strCity != details.get('City', '').strip():
-        file.write(strNumber + ',' + strInfos + ',\n')
-        return
-    if strIsp != details.get('Corp', '').strip().lstrip('中国'):
-        file.write(strNumber + ',' + strInfos + ',\n')
-        return
+        return (False, 'update,' + strNumber + ',' + strInfos + ',\n')
+    #if strIsp != details.get('TO', '').strip().lstrip('中国'):
+    #    return (False, 'update,' + strNumber + ',' + strInfos + ',\n')
 
-    cursor.execute('update T_PhoneLocation set prefix = ?, province = ?, city = ?, isp = ?, code = ?, zip = ?, types = ? where phone = ?;',
-                   (strNumber[0:3],
-                    strProvince,
-                    strCity,
-                    strIsp,
-                    details.get('AreaCode', ''),
-                    details.get('PostCode', ''),
-                    getTypes(details),
-                    strNumber))
+    strSql = "update T_PhoneLocation set prefix = '%s', province = '%s', city = '%s', isp = '%s', code = '%s', zip = '%s', types = '%s' where phone = '%s';" % (strNumber[0:3],
+                 strProvince,
+                 strCity,
+                 strIsp,
+                 details.get('AreaCode', ''),
+                 details.get('PostCode', ''),
+                 getTypes(details),
+                 strNumber)
+
+    return (True, strSql)
 
 #prefix, phone, province, city, isp, code, zip, types
-def insertRecord(cursor, strNumber, strInfos, file):
+def insertRecord(strNumber, strInfos):
     if len(strInfos.split('#')) != 3:
         strTypes = strInfos.strip().lstrip('【').rstrip('】')
-        cursor.execute('insert into T_PhoneLocation values (?, ?, ?, ?, ?, ?, ?, ?);',
-                       (strNumber, strNumber, '', '', '', '', '', strTypes))
-        return
+        strSql = "insert into T_PhoneLocation values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (strNumber, strNumber, '', '', '', '', '', strTypes)
+        return (True, strSql)
 
     details = queryNumber(strNumber)
     if details.get('QueryResult', 'False') == 'False':
-        file.write(strNumber + ',' + strInfos + ',\n')
-        return
+        #file.write(strNumber + ',' + strInfos + ',\n')
+        return (False, 'insert,' + strNumber + ',' + strInfos + ',\n')
 
     strProvince, strCity, strIsp = strInfos.split('#')
     if strProvince != details.get('Province', '').strip():
-        file.write(strNumber + ',' + strInfos + ',\n')
-        return
+        return (False, 'insert,' + strNumber + ',' + strInfos + ',\n')
     if strCity != details.get('City', '').strip():
-        file.write(strNumber + ',' + strInfos + ',\n')
-        return
-    if strIsp != details.get('Corp', '').strip().lstrip('中国'):
-        file.write(strNumber + ',' + strInfos + ',\n')
-        return
-    cursor.execute('insert into T_PhoneLocation values (?, ?, ?, ?, ?, ?, ?, ?);',
-                   (strNumber[0:3],
+        return (False, 'insert,' + strNumber + ',' + strInfos + ',\n')
+    #if strIsp != details.get('Corp', '').strip().lstrip('中国'):
+    #    return (False, 'insert,' + strNumber + ',' + strInfos + ',\n')
+
+    strSql = "insert into T_PhoneLocation values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (strNumber[0:3],
                     strNumber,
                     strProvince,
                     strCity,
                     strIsp,
                     details.get('AreaCode', ''),
                     details.get('PostCode', ''),
-                    getTypes(details)))
+                    getTypes(details))
+
+    return (True, strSql)
 
 
-bRunning = True
 
-lock = threading.Lock()
 
 class Producer(threading.Thread):
-    def __init__(self, name, queue):
-        self.queue = queue
+    def __init__(self, name, inQ, outQ):
+        self.inQ = inQ
+        self.outQ = outQ
         threading.Thread.__init__(self, name=name)
 
     def run(self):
+        while True:
+            with lock:
+                if not bRunning:
+                    break
+            if self.inQ.empty():
+                continue
+            #print('[%s]Size of inQ: %d' % (threading.current_thread().name, self.inQ.qsize()))
+            qData = self.inQ.get()
+            strNumber = qData[0]
+            strInfos = qData[1]
+            values = qData[2]
+            #strExcelInfos = "%s#%s" % (strInfos.split('#')[0], strInfos.split('#')[1])
+            if len(values) != 0:
+                strDBInfos = "%s#%s#%s" % (
+                    values[0][0].strip(), values[0][1].strip(), values[0][2].strip())
+                if strDBInfos != strInfos:
+                    if len(strInfos.split('#')) == 3:
+                        #file.write(strNumber + ',' + strInfos + ',' + strDBInfos + '\n')
+                        self.outQ.put(updateRecord(strNumber, strInfos))
+
+            else:
+                #file.write(strNumber + ',' + strInfos + ',\n')
+                self.outQ.put(insertRecord(strNumber, strInfos))
 
 
-        global lock
-        global bRunning
-        with lock:
-            bRunning = False
+
 
 
 if __name__ == '__main__':
@@ -121,7 +191,16 @@ if __name__ == '__main__':
     #queryNumber('5700523')
 
     startTime = time.time()
-    conn = sqlite3.connect('phonesqlite.db')
+    inQ = queue.Queue()
+    outQ = queue.Queue()
+
+    producerList = []
+    for i in range(2):
+        p = Producer(name='producer %d' % (i + 1,), inQ=inQ, outQ=outQ)
+        p.start()
+        producerList.append(p)
+
+    conn = sqlite3.connect('/mnt/hgfs/Share/phonesqlite.db')
     print('Open database successfully')
     cursor = conn.cursor()
 
@@ -130,12 +209,14 @@ if __name__ == '__main__':
     values = cursor.fetchall()
     print('Total DB Rows: ', values[0][0])
 
-    file = open('log.csv', 'w')
-    file.write('number,excel,db\n')
+    file = open('/mnt/hgfs/Share/log-%d.csv' % (time.time(),), 'w')
+    file.write('type,number,excel,db\n')
 
     data = xlrd.open_workbook('phonelocation.xlsx')
+    #data = xlrd.open_workbook('/mnt/hgfs/Share/test.xlsx')
     table = data.sheets()[0]
     print('Total Excel Rows: ', table.nrows)
+
     for row in range(table.nrows):
     #for row in range(1):
         strNumber = table.cell(row, 0).value
@@ -145,6 +226,11 @@ if __name__ == '__main__':
         cursor.execute(strSql)
         values = cursor.fetchall()
 
+        qData = [strNumber, strInfos, values]
+
+        inQ.put(qData)
+
+        '''
         if len(values) != 0:
             strDBInfos = "%s#%s#%s" % (values[0][0].strip(), values[0][1].strip(), values[0][2].strip())
             if strDBInfos != strInfos:
@@ -153,7 +239,43 @@ if __name__ == '__main__':
         else:
             #file.write(strNumber + ',' + strInfos + ',\n')
             insertRecord(cursor, strNumber, strInfos, file)
+        '''
+
+
+    while True:
+        if inQ.empty():
+            with lock:
+                bRunning = False
+                break
+
+        if outQ.empty():
+            continue
+
+        ret = outQ.get()
+
+        if not ret[0]:
+            #print(ret[1])
+            file.write(ret[1])
+        else:
+            #print(ret[1])
+            cursor.execute(ret[1])
+
+
+    while not outQ.empty():
+        ret = outQ.get()
+
+        if not ret[0]:
+            #print(ret[1])
+            file.write(ret[1])
+        else:
+            #print(ret[1])
+            cursor.execute(ret[1])
+
+
+    for producer in producerList:
+        producer.join()
 
     file.close()
+    conn.commit()
     conn.close()
     print('Run time: %fs' % (time.time() - startTime))
