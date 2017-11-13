7e3e6ae95b174e1854a46ed5c89efd3fd629cfd2
Ubuntu <youthdou@126.com>
2017-11-13 11:49:14 +0800

    modify code

--- learning/csv/csv_reader.py
+++ learning/csv/csv_reader.py
@@ -6,10 +6,11 @@ data = requests.get('http://pythonscraping.com/files/MontyPythonAlbums.csv')
 dataFile = StringIO(data.content.decode('utf-8'))
 
 #csvReader = csv.reader(dataFile)
+#for row in csvReader:
 dictReader = csv.DictReader(dataFile)
+
 print(dictReader.fieldnames)
 
-#for row in csvReader:
 for row in dictReader:
     print(row)
 
--- scrapy/toys/hupu-nba.py
+++ scrapy/toys/hupu-nba.py
@@ -11,6 +11,7 @@ def getText(bsObj, strSelector):
         return infos[0].text
     return strInfo
 
+'''
 def parseUrl(strUrl):
     r = requests.get(strUrl)
     bsObj = BeautifulSoup(r.text, 'html5lib')
@@ -36,7 +37,29 @@ def parseUrl(strUrl):
                                  '> div.message > p > a')
 
     return '[%s]%s [%s] [%s]%s' % (team_a_name, team_a_score, game_info, team_b_name, team_b_score)
+'''
 
+def getTeamInfo(bsObj, strTeam):
+    div = bsObj.find('div', strTeam)
+    name = div.img['alt']
+    score = div.h2.text 
+    return (name, score)
+
+def getGameInfo(bsObj):
+    div = bsObj.find('div', {'class':'team_num'})
+    if div is None:
+        return ''
+    return div.text
+
+def parseUrl(strUrl):
+    r = requests.get(strUrl)
+    bsObj = BeautifulSoup(r.text, 'html5lib')
+    #team_a = bsObj.find_all('div', {'class':'team_a'})
+    team_a = getTeamInfo(bsObj, 'team_a')
+    team_b = getTeamInfo(bsObj, 'team_b')
+    game = getGameInfo(bsObj)
+
+    return '[%s]%s [%s] [%s]%s' % (team_a[0], team_a[1], game, team_b[0], team_b[1])
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
